f0c150b8a3ad17a1dff918381e305020ce2b195d
imran9663 <imranhussian9663@gmail.com>
2023-01-02 18:54:05 +0530

    adding PWA configuration's

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import RoundPlayBtn from './components/RoundPlayBtn';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Drag from './components/RoundPlayBtn';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
@@ -21,6 +21,7 @@ root.render(
 
 );
 
+serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

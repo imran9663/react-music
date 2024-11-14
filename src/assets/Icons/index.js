f1eecd91fa866e6086d50a6d6b500a8790ce7219
imran9663 <imranhussian9663@gmail.com>
2024-11-14 17:52:20 +0530

    adding react lazy to svg Components

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -1,58 +1,5 @@
 
-import { ReactComponent as AiFillCrown } from "./AiFillCrown.svg";
-import { ReactComponent as AiOutlineCloseCircle } from "./AiOutlineCloseCircle.svg";
-import { ReactComponent as AiOutlineUser } from "./AiOutlineUser.svg";
-import { ReactComponent as AiFillRightCircle } from "./AiFillRightCircle.svg";
-import { ReactComponent as BiArrowBack } from "./BiArrowBack.svg";
-import { ReactComponent as BiAddToQueue } from "./BiAddToQueue.svg";
-import { ReactComponent as BiAlbum } from "./BiAlbum.svg";
-import { ReactComponent as BiUserCircle } from "./BiUserCircle.svg";
-import { ReactComponent as BsChevronDoubleDown } from "./BsChevronDoubleDown.svg";
-import { ReactComponent as BsChevronDoubleUp } from "./BsChevronDoubleUp.svg";
-import { ReactComponent as BsVolumeMute } from "./BsVolumeMute.svg";
-import { ReactComponent as BsVolumeUp } from "./BsVolumeUp.svg";
-import { ReactComponent as BsVolumeDown } from "./BsVolumeDown.svg";
-import { ReactComponent as BsHeart } from "./BsHeart.svg";
-import { ReactComponent as BsHeartFill } from "./BsHeartFill.svg";
-import { ReactComponent as BsArrowRepeat } from "./BsArrowRepeat.svg";
-import { ReactComponent as BsShuffle } from "./BsShuffle.svg";
-import { ReactComponent as BsPlayFill } from "./BsPlayFill.svg";
-import { ReactComponent as BsPauseFill } from "./BsPauseFill.svg";
-import { ReactComponent as BsChevronBarLeft } from "./BsChevronBarLeft.svg";
-import { ReactComponent as BsChevronBarRight } from "./BsChevronBarRight.svg";
-import { ReactComponent as BsHeadphones } from "./BsHeadphones.svg";
-import { ReactComponent as BsArrowLeft } from "./BsArrowLeft.svg";
-import { ReactComponent as BsThreeDotsVertical } from "./BsThreeDotsVertical.svg";
-import { ReactComponent as BsCloudDownloadFill } from "./BsCloudDownloadFill.svg";
-import { ReactComponent as BsMusicNote } from "./BsMusicNote.svg";
-import { ReactComponent as BsArrowRight } from "./BsArrowRight.svg";
-import { ReactComponent as FiHome } from "./FiHome.svg";
-import { ReactComponent as FiSearch } from "./FiSearch.svg";
-import { ReactComponent as FaFacebookF } from "./FaFacebookF.svg";
-import { ReactComponent as FaTwitter } from "./FaTwitter.svg";
-import { ReactComponent as FaWikipediaW } from "./FaWikipediaW.svg";
-import { ReactComponent as GoVerified } from "./GoVerified.svg";
-import { ReactComponent as HiLanguage } from "./HiLanguage.svg";
-import { ReactComponent as RxDot } from "./RxDot.svg";
-import { ReactComponent as RxDragHandleDots2 } from "./RxDragHandleDots2.svg";
-import { ReactComponent as SiReact } from "./SiReact.svg";
-import { ReactComponent as SlPlaylist } from "./SlPlaylist.svg";
-import { ReactComponent as TfiLoop } from "./TfiLoop.svg";
-import { ReactComponent as IoPlaySkipForward } from "./IoPlaySkipForward.svg";
-import { ReactComponent as BsTrashCan } from "./BsTrashCan.svg";
-import { ReactComponent as BsEyeFill } from "./BsEyeFill.svg";
-import { ReactComponent as BsEyeSlashFill } from "./BsEyeSlashFIll.svg";
-import { ReactComponent as BsRadioPin } from "./BsRadioPin.svg";
-import { ReactComponent as BsDownload } from "./BsDownload.svg";
-import { ReactComponent as BsShare } from "./BsShare.svg";
-import { ReactComponent as BsListPlus } from "./BsListPlus.svg";
-import { ReactComponent as BsListPlay } from "./BsListPlay.svg";
-import { ReactComponent as BsPlusCircle } from "./BsPlusCircle.svg";
-import { ReactComponent as BsNoMusic } from "./BsNoMusic.svg";
-import { ReactComponent as BsLyricsNote } from "./BsLyricsNote.svg";
-
-
-
+import React from "react"
 import defualtImage from "./defualtImage.svg";
 import mello from "./mello.svg";
 import nointernet from "./no_internet.svg";
@@ -63,6 +10,61 @@ import teamFun from "./teamFun.svg";
 import mailbox from "./mailbox.svg";
 import ForgotPassword from "./forgot_password.svg";
 import SadSitting from "./sadSitting.svg";
+const AiFillCrown = React.lazy(() => import("./AiFillCrown.svg").then(module => ({ default: module.ReactComponent })))
+const AiOutlineCloseCircle = React.lazy(() => import("./AiOutlineCloseCircle.svg").then(module => ({ default: module.ReactComponent })))
+const AiOutlineUser = React.lazy(() => import("./AiOutlineUser.svg").then(module => ({ default: module.ReactComponent })))
+const AiFillRightCircle = React.lazy(() => import("./AiFillRightCircle.svg").then(module => ({ default: module.ReactComponent })))
+const BiArrowBack = React.lazy(() => import("./BiArrowBack.svg").then(module => ({ default: module.ReactComponent })))
+const BiAddToQueue = React.lazy(() => import("./BiAddToQueue.svg").then(module => ({ default: module.ReactComponent })))
+const BiAlbum = React.lazy(() => import("./BiAlbum.svg").then(module => ({ default: module.ReactComponent })))
+const BiUserCircle = React.lazy(() => import("./BiUserCircle.svg").then(module => ({ default: module.ReactComponent })))
+const BsChevronDoubleDown = React.lazy(() => import("./BsChevronDoubleDown.svg").then(module => ({ default: module.ReactComponent })))
+const BsChevronDoubleUp = React.lazy(() => import("./BsChevronDoubleUp.svg").then(module => ({ default: module.ReactComponent })))
+const BsVolumeMute = React.lazy(() => import("./BsVolumeMute.svg").then(module => ({ default: module.ReactComponent })))
+const BsVolumeUp = React.lazy(() => import("./BsVolumeUp.svg").then(module => ({ default: module.ReactComponent })))
+const BsVolumeDown = React.lazy(() => import("./BsVolumeDown.svg").then(module => ({ default: module.ReactComponent })))
+const BsHeart = React.lazy(() => import("./BsHeart.svg").then(module => ({ default: module.ReactComponent })))
+const BsHeartFill = React.lazy(() => import("./BsHeartFill.svg").then(module => ({ default: module.ReactComponent })))
+const BsArrowRepeat = React.lazy(() => import("./BsArrowRepeat.svg").then(module => ({ default: module.ReactComponent })))
+const BsShuffle = React.lazy(() => import("./BsShuffle.svg").then(module => ({ default: module.ReactComponent })))
+const BsPlayFill = React.lazy(() => import("./BsPlayFill.svg").then(module => ({ default: module.ReactComponent })))
+const BsPauseFill = React.lazy(() => import("./BsPauseFill.svg").then(module => ({ default: module.ReactComponent })))
+const BsChevronBarLeft = React.lazy(() => import("./BsChevronBarLeft.svg").then(module => ({ default: module.ReactComponent })))
+const BsChevronBarRight = React.lazy(() => import("./BsChevronBarRight.svg").then(module => ({ default: module.ReactComponent })))
+const BsHeadphones = React.lazy(() => import("./BsHeadphones.svg").then(module => ({ default: module.ReactComponent })))
+const BsArrowLeft = React.lazy(() => import("./BsArrowLeft.svg").then(module => ({ default: module.ReactComponent })))
+const BsThreeDotsVertical = React.lazy(() => import("./BsThreeDotsVertical.svg").then(module => ({ default: module.ReactComponent })))
+const BsCloudDownloadFill = React.lazy(() => import("./BsCloudDownloadFill.svg").then(module => ({ default: module.ReactComponent })))
+const BsMusicNote = React.lazy(() => import("./BsMusicNote.svg").then(module => ({ default: module.ReactComponent })))
+const BsArrowRight = React.lazy(() => import("./BsArrowRight.svg").then(module => ({ default: module.ReactComponent })))
+const FiHome = React.lazy(() => import("./FiHome.svg").then(module => ({ default: module.ReactComponent })))
+const FiSearch = React.lazy(() => import("./FiSearch.svg").then(module => ({ default: module.ReactComponent })))
+const FaFacebookF = React.lazy(() => import("./FaFacebookF.svg").then(module => ({ default: module.ReactComponent })))
+const FaTwitter = React.lazy(() => import("./FaTwitter.svg").then(module => ({ default: module.ReactComponent })))
+const FaWikipediaW = React.lazy(() => import("./FaWikipediaW.svg").then(module => ({ default: module.ReactComponent })))
+const GoVerified = React.lazy(() => import("./GoVerified.svg").then(module => ({ default: module.ReactComponent })))
+const HiLanguage = React.lazy(() => import("./HiLanguage.svg").then(module => ({ default: module.ReactComponent })))
+const RxDot = React.lazy(() => import("./RxDot.svg").then(module => ({ default: module.ReactComponent })))
+const RxDragHandleDots2 = React.lazy(() => import("./RxDragHandleDots2.svg").then(module => ({ default: module.ReactComponent })))
+const SiReact = React.lazy(() => import("./SiReact.svg").then(module => ({ default: module.ReactComponent })))
+const SlPlaylist = React.lazy(() => import("./SlPlaylist.svg").then(module => ({ default: module.ReactComponent })))
+const TfiLoop = React.lazy(() => import("./TfiLoop.svg").then(module => ({ default: module.ReactComponent })))
+const IoPlaySkipForward = React.lazy(() => import("./IoPlaySkipForward.svg").then(module => ({ default: module.ReactComponent })))
+const BsTrashCan = React.lazy(() => import("./BsTrashCan.svg").then(module => ({ default: module.ReactComponent })))
+const BsEyeFill = React.lazy(() => import("./BsEyeFill.svg").then(module => ({ default: module.ReactComponent })))
+const BsEyeSlashFill = React.lazy(() => import("./BsEyeSlashFIll.svg").then(module => ({ default: module.ReactComponent })))
+const BsRadioPin = React.lazy(() => import("./BsRadioPin.svg").then(module => ({ default: module.ReactComponent })))
+const BsDownload = React.lazy(() => import("./BsDownload.svg").then(module => ({ default: module.ReactComponent })))
+const BsShare = React.lazy(() => import("./BsShare.svg").then(module => ({ default: module.ReactComponent })))
+const BsListPlus = React.lazy(() => import("./BsListPlus.svg").then(module => ({ default: module.ReactComponent })))
+const BsListPlay = React.lazy(() => import("./BsListPlay.svg").then(module => ({ default: module.ReactComponent })))
+const BsPlusCircle = React.lazy(() => import("./BsPlusCircle.svg").then(module => ({ default: module.ReactComponent })))
+const BsNoMusic = React.lazy(() => import("./BsNoMusic.svg").then(module => ({ default: module.ReactComponent })))
+const BsLyricsNote = React.lazy(() => import("./BsLyricsNote.svg").then(module => ({ default: module.ReactComponent })))
+
+
+
+
 
 export const Icons = {
     notFound: notFound,
